d14c75e0be85a8851400af6c99e6af3a608c7387
Sebastian Kluge <sebastian.kluge@posteo.de>
2023-12-21 20:34:38 +0100

    Download Pdf hinzugefügt

--- src/components/Klassenzimmer.js
+++ src/components/Klassenzimmer.js
@@ -4,6 +4,9 @@ import Schueler from "./Schueler";
 import {useEffect, useState} from "react";
 import axios from "axios";
 import {useCurrentStudent} from "./CurrentStudentContext";
+import * as htmlToImage from 'html-to-image';
+import { jsPDF } from "jspdf";
+import { toPng } from 'html-to-image';
 
 export default function Klassenzimmer(props) {
     // Erlaube Zugriff auf KameraView
@@ -43,6 +46,9 @@ export default function Klassenzimmer(props) {
                             <Reihe id={3} getSchuelerByPosition={getSchuelerByPosition}></Reihe>
                             <Reihe id={4} getSchuelerByPosition={getSchuelerByPosition}></Reihe>
                             <Lehrkraft></Lehrkraft>
+                        </div>
+                        <div className={"downloadButton"}>
+                            <button onClick={downloadPDF}>Download Pdf</button>
                         </div>
                     </>
                 )
@@ -92,5 +98,39 @@ function fetchKlassenzimmer(setKlassenzimmer) {
         })
         .catch(error => {
             console.error("Error fetching klassenzimmer:", error);
+        });
+}
+
+function downloadPDF() {
+    const klassenzimmer = document.getElementsByClassName('Klassenzimmer')[0];
+    if (klassenzimmer) {
+        console.log("Klassenzimmer ist da.")
+    }
+    const {offsetWidth, offsetHeight} = klassenzimmer;
+    htmlToImage.toPng(klassenzimmer, {
+        width: offsetWidth,
+        height: offsetHeight,
+        canvasWidth: 1125,
+        canvasHeight: 800,
+        skipAutoScale: false,
+        style: {
+            margin: 0,
+            padding: 0,
+        },
+        quality: 0.95
+    })
+        .then(function (dataUrl) {
+            var link = document.createElement('a');
+            link.download = 'my-image-name.jpeg';
+            const options = {orientation: 'l', format: 'a4'}
+            const pdf = new jsPDF(options);
+            const imgProps = pdf.getImageProperties(dataUrl);
+            const pdfWidth = pdf.internal.pageSize.getWidth();
+            const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+            pdf.addImage(dataUrl, 'PNG', 0, 0);
+            pdf.save("download.pdf");
+        })
+        .catch(function (error) {
+            console.error("Error in htmlToPng", error);
         });
 }
