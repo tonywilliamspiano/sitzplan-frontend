c1d0ce928cc843cfec96e59407411be63d1573c3
Sebastian Kluge <sebastian.kluge@posteo.de>
2023-12-28 15:02:12 +0100

    hinzugefügt Funktion zum Word Dokument einlesen

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -31,6 +31,47 @@ export default function Navbar(props) {
         setZimmerHinzufuegen(true);
     }
 
+    async function klassenlisteHochladen(){
+        // Optionen setzen sodass nur die Worddokumente angezeigt werden.
+        const pickerOpts = {
+            types: [
+                {
+                    description: "Word Dokumente",
+                    accept: {
+                        "text/*": [".docx"],
+                    },
+                },
+            ],
+            excludeAcceptAllOption: true,
+            multiple: false,
+        };
+
+        const pizzip = require("pizzip");
+        // const docxtemplater = require("docxtemplater");
+        const convert = require('xml-js')
+        try {
+            // Open file picker and destructure the result the first handle
+            const [fileHandle] = await window.showOpenFilePicker(pickerOpts);
+            // get file contents
+            const fileData = await fileHandle.getFile();
+            const content = await fileData.arrayBuffer();
+            const unzipped = new pizzip(content);
+            const rawJSON = convert.xml2json(unzipped.files["word/document.xml"].asText(), {compact: true, spaces: 4});
+            const convertedJSON = JSON.parse(rawJSON);
+            console.log(convertedJSON['w:document']['w:body']['w:tbl']['w:tr']);
+            const rows = convertedJSON['w:document']['w:body']['w:tbl']['w:tr'];
+            for (let i = 0; i < rows.length; i++) {
+                const surname = rows[i]['w:tc'][1]['w:p']['w:r']['w:t']['_text'];
+                const name = rows[i]['w:tc'][2]['w:p']['w:r']['w:t']['_text'];
+                console.log(name + " " + surname);
+            }
+
+        } catch (error){
+            console.error("Error:", error.message);
+        }
+
+    }
+
     return (
         <>
             <div id="mySidenav" className="sidenav">
@@ -46,7 +87,8 @@ export default function Navbar(props) {
 
 
                 <div className="navItem" onClick={() => {}}>Klassenliste Bearbeiten</div>
-                <div className="navItem" onClick={() => {}}>Klassenliste Hochladen</div>
+                <div className="navItem"
+                     onClick={klassenlisteHochladen}>Klassenliste Hochladen</div>
 
                 <div className="navItem"></div>
                 <div className="navItem"></div>
